6b3dd6c4cbc0a3312a59494997609d58fe6ca1a4
keinstein <keinstein>
2004-07-19 22:14:16 +0000

    globale Variablen php_self, get und post initialisieren

--- klassenliste/index.php
+++ klassenliste/index.php
@@ -1,4 +1,4 @@
-<?php /* $Id: index.php,v 1.6 2004/07/14 23:58:15 keinstein Exp $
+<?php /* $Id: index.php,v 1.7 2004/07/19 22:14:16 keinstein Exp $
 Copyright (c) 2000 SchlemmerSoft (Tobias Schlemmer)
 
 This program is free software; you can redistribute it and/or
@@ -18,11 +18,23 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 Der Autor kann unter der Email: Tobias.Schlemmer@web.de erreicht werden.
 
 $Log: index.php,v $
+Revision 1.7  2004/07/19 22:14:16  keinstein
+globale Variablen php_self, get und post initialisieren
+
 Revision 1.6  2004/07/14 23:58:15  keinstein
 Minimale Aufrufmöglichkeit des Skriptes (standalone)
 
 
 */ 
+
+/* Referenz auf Formular-Variablen setzen */
+/* Falls die Variablen künstlich verändert werden sollen: "&" weglassen */
+
+$GLOBALS['mod_absolventen_glob']['php_self']=$_SERVER['PHP_SELF'];
+
+$GLOBALS['mod_absolventen_glob']['get']=&$_GET;
+$GLOBALS['mod_absolventen_glob']['post']=&$_POST;
+
 chdir('includes');
 require('main.inc');
 
